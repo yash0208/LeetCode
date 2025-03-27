98827c4df41812602cf6d6199d2f900cd6be7343
yash0208 <yashrajmehta3027@gmail.com>
2025-03-27 00:09:04 -0400

    binary search

--- src/BinarySearch/81.search-in-rotated-sorted-array-ii.java
+++ src/BinarySearch/81.search-in-rotated-sorted-array-ii.java
@@ -8,35 +8,35 @@ package BinarySearch;
 // @lc code=start
 class Solution {
     public boolean search(int[] nums, int target) {
-        // we want to find the target in the rotated sorted array
-        // we can use binary search to find the target
-        // we can use the same approach as in the rotated sorted array
-        int left = 0;
-        int right = nums.length - 1;
-        
+        int left = 0, right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] == target) return true;
-            // now we have to think all test cases
-            // lets think when mid is same as right 
-            // 
-            if( nums[mid] == nums [right]) right --;
-            // lets see if the left is sorted
-            else if ( nums[left] < nums[mid] ){
-                // if so there can be 2 cases 
-                // number is in left 
-                if(target < nums[mid]) right = mid - 1;
-                else left = mid + 1;
+
+            if (nums[mid] == target) return true;
+
+            // Handle duplicates: shrink search space
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
             }
-            // else the right part is sorted
-            else { 
-                if (nums[mid] < target && target <= nums[right]) 
-                    left = mid + 1;
-                else 
-                    right = mid - 1;
+            // Left half is sorted
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1; // Search left half
+                } else {
+                    left = mid + 1; // Search right half
+                }
+            }
+            // Right half is sorted
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1; // Search right half
+                } else {
+                    right = mid - 1; // Search left half
+                }
             }
         }
-
         return false;
     }
 }
